b61f3ebd7524ce1ef1d5e33901572ae7e71d6df0
Jake Gundersen <fattjake@gmail.com>
2016-07-21 09:39:13 -0600

    Initial Commit

---
+++ LiveVideoCoreImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveVideoCoreImage
+//
+//  Created by Jake Gundersen on 7/21/16.
+//  Copyright © 2016 Third Rail, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
